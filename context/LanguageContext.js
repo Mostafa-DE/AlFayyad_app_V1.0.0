b59f71569a69a4382179026ddb7e9ed35a96bf3c
Mostafa-DE <mostafafayyado1@gmail.com>
2022-02-13 22:01:50 +0200

    put language state in local storage to save the language user select :)

--- context/LanguageContext.js
+++ context/LanguageContext.js
@@ -1,9 +1,21 @@
-import { createContext, useState } from "react";
+import {createContext, useEffect, useState} from "react";
 
 export const LanguageContext = createContext();
 
 export const LanguageProvider = ({ children }) => {
   const [language, setLanguage] = useState("arabic");
+
+    /*---------Save Product Cart in localStorage-----------*/
+
+  useEffect(() => {
+    setLanguage(window.localStorage.getItem("language"));
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("language", language);
+  }, [language]);
+
+  /*-----------------------X----------------------------*/
 
   const ChangeToEnglishLanguage = () => {
     setLanguage("english");
